22558f6c13a2aefbad62c063438b08d1c90724f6
asem-kurd <asem.vip10@gmail.com>
2025-04-28 20:43:05 +0300

    Adding Navigation Menu & Footer to Master Page #AK

---
+++ Site1.Master.cs
@@ -0,0 +1,19 @@
+﻿namespace ElibraryManagementSystem
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="Site1" />
+    /// </summary>
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        /// <summary>
+        /// The Page_Load
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿namespace ElibraryManagementSystem
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="WebForm1" />
+    /// </summary>
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        /// <summary>
+        /// The Page_Load
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/></param>
+        /// <param name="e">The e<see cref="EventArgs"/></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
